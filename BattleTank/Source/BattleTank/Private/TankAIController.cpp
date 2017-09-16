497e83096270cbb5e6cf20b11cde505992515837
RuanPC\theronrp <ruan.theron@yahoo.com>
2017-09-16 18:08:29 +0200

    Added AI controller and made AI players find first person player

---
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "../Public/TankAIController.h"
+#include "Engine/World.h"
+
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIController not possessing a tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIController possessing: %s"), *(ControlledTank->GetName()));
+	}
+
+	auto PlayerTank = GetPlayerTank();
+	if (!PlayerTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIController could not find player"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("I found you!!! %s"), *(PlayerTank->GetName()));
+	}
+}
+
+ATank * ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (!PlayerPawn) { return nullptr; }
+	return Cast<ATank>(PlayerPawn);
+}
